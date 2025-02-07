a7441d745cdab5b9e137038d671b5e199103501c
Renato Fioroni Simoes <renatofsim@gmail.com>
2025-02-07 10:45:59 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		
-		Department object = new Department(1, "Books");
+		Department dep = new Department(1, "Books");
+		
+		Seller sel = new Seller(33, "Alex", "alex@gmail.com", new Date(), 3000.0, dep);
 
-		System.out.println(object.toString());
+		System.out.println(dep.toString() + "\n" + sel.toString());
 	}
 
 }
