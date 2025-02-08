effacf8b10859d98c226beff402532b5175375a6
Renato Fioroni Simoes <renatofsim@gmail.com>
2025-02-08 11:15:19 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.List;
 import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,11 +14,17 @@ public class Program {
 		Locale.setDefault(Locale.US);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println("---- Seller findById ----");
+
+		System.out.println("---- 1: Seller findById ----");
 		Seller seller = sellerDao.findById(4);
-		
-		System.out.println(seller);
+		System.out.println(seller.toString());
+
+		System.out.println("\n---- 2: Seller findByDepartment ----");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj.toString());
+		}
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -61,6 +64,49 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 
 	}
+	
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " 
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id " 
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();//Utilizando um map para não criar instâncias
+														 //diferentes de um mesmo departamento
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) { //Checa se o departamento já existe, se não, o instancia e salva no map
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller seller = instatiateSeller(rs, dep);
+				list.add(seller);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+	
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
 
 	private Seller instatiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller seller = new Seller();
@@ -78,11 +124,6 @@ public class SellerDaoJDBC implements SellerDao {
 		dep.setId(rs.getInt("DepartmentId"));
 		dep.setName(rs.getString("DepName"));
 		return dep;
-	}
-
-	@Override
-	public List<Seller> findAll() {
-		return null;
 	}
 
 }
